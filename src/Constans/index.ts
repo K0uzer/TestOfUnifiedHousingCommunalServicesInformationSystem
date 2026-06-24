77220e29120fbb7ed17f9d654fae19317f36e513
Alex <87208475+K0uzer@users.noreply.github.com>
2026-06-24 22:37:15 +0300

    refactor: constants (URL, URI, API_BASE)

--- src/Constans/index.ts
+++ src/Constans/index.ts
@@ -1,6 +1,9 @@
 // Fetch
 const URL = 'https://showroom.eis24.me';
-const URN = '/c300/api/v4/test/meters';
-const URI = URL + URN;
+const URN = '/c300/api/v4/test';
 
-export { URI };
+const API_BASE = URL + URN;
+
+export const METERS_URL = `${API_BASE}/meters/`;
+export const AREAS_URL = `${API_BASE}/areas/`;
+export const PAGE_SIZE = 20;
