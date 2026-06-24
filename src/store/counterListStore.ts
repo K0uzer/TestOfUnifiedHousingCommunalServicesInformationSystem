2e61991a24770acab00e926020bf2b752b6bf33f
Alex <87208475+K0uzer@users.noreply.github.com>
2026-06-24 15:24:46 +0300

    feat: create CounterListStore

---
+++ src/store/counterListStore.ts
@@ -0,0 +1,24 @@
+import { makeAutoObservable } from 'mobx';
+import {} from '../constans';
+
+class CounterListStore {
+  list: string[] = [];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  getList(page: number): string[] {
+    return this.list.slice(0 + page, 20);
+  }
+
+  lengthList() {
+    return this.list.length;
+  }
+
+  loadList(counterlist: string[]) {
+    this.list = [...counterlist];
+  }
+}
+
+export const counterListStore = new CounterListStore();
