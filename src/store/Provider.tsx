0264b3f11c3907992bf77f1976f57fe6a485bef2
Alex <87208475+K0uzer@users.noreply.github.com>
2026-06-24 22:19:28 +0300

    fix: provider meterStore

--- src/store/Provider.tsx
+++ src/store/Provider.tsx
@@ -1,11 +1,13 @@
 import { type ReactNode } from 'react';
 import { Provider } from 'mobx-react';
+import { meterStore } from './meterStore';
 
 interface IProviderComponentProps {
   children: ReactNode;
 }
 
 const ProviderComponent = ({ children }: IProviderComponentProps) => (
-  <Provider>{children}</Provider>
+  <Provider meterStore={meterStore}>{children}</Provider>
 );
+
 export default ProviderComponent;
