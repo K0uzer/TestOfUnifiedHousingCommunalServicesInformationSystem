d96d6c664f9134b86f38ada86ae64159a727ee00
Alex <87208475+K0uzer@users.noreply.github.com>
2026-06-25 09:57:16 +0300

    refacror: change tsconfig.app - add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-svg-loader';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@constans': path.resolve(__dirname, './src/constans'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@stypes': path.resolve(__dirname, './src/stypes'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    }
+  },
   plugins: [react(), svgr()],
 });
